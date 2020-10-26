f370db2bf440a90794d89b65a9c561e7e3550d6b
RakibAhmedPranto <rakib820@gmail.com>
2020-10-26 20:56:24 +0600

    day 2

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,16 @@ Route::group(['prefix' => 'admin'], function () {
     Route::get('/','Backend\DashboardController@index')->name('admin.dashboard');
     Route::resource('roles', 'Backend\RolesController', ['names' => 'admin.roles']);
     Route::resource('users', 'Backend\UsersController', ['names' => 'admin.users']);
+
+    //login Route
+    Route::get('/login','Backend\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login/submit','Backend\Auth\LoginController@login')->name('admin.login.submit');
+    //Logout Route
+    Route::post('/logout/submit','Backend\Auth\LoginController@logout')->name('admin.logout.submit');
+
+    //forget Pass route
+    Route::get('/password/reset','Backend\Auth\ForgetPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset/submit','Backend\Auth\ForgetPasswordController@reset')->name('admin.password.update');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
